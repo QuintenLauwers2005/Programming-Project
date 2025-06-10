18b696aac545ccffbdfca16bbaff546f41ecf5a6
MarwanMakouh <marwan.makouh@student.ehb.be>
2025-06-10 13:49:15 +0200

    School database linken

--- Project-Code/Programming-project/backend/server.js
+++ Project-Code/Programming-project/backend/server.js
@@ -3,18 +3,29 @@ const cors = require('cors')
 const mysql = require('mysql2')
 const app = express()
 const port = 5000
+require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
 
 // aanpassen als de database verandert
-const db = mysql.createConnection({
+/*const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'carrierlauch'
   
-})
+})*/
+
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+
 
 db.connect(err => {
   if (err) throw err
