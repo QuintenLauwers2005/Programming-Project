b7c02b74ca7a403a4d5564ef845b7bc644deaa3c
Argo216 <nday.roman@gmail.com>
2025-06-10 11:08:50 +0200

    melding knop

--- Project-Code/Programming-project/frontend/src/Components/Navbar.jsx
+++ Project-Code/Programming-project/frontend/src/Components/Navbar.jsx
@@ -1,17 +1,61 @@
 import './Navbar.css'
-
+import React, { useState, useRef, useEffect } from "react";
 import Logo from './Logo'
 import { useNavigate } from 'react-router-dom';
 
 function Navbar(){
 
   const navigate = useNavigate();
+
+    const [showNotifications, setShowNotifications] = useState(false);
+    const buttonRef = useRef(null);
+  const popoutRef = useRef(null);
+
+  const toggleNotifications = () => {
+    setShowNotifications((prev) => !prev);
+  };
+
+const handleClickOutside = (event) => {
+  if (
+    popoutRef.current &&
+    !popoutRef.current.contains(event.target) &&
+    !buttonRef.current.contains(event.target)
+  ) {
+    setShowNotifications(false);
+  }
+};
+
+
+    if (showNotifications) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+   
+
     return(
          <div>
       <div className="top-bar">
         <button className="login-btn" onClick={() => navigate('/login')}>Login</button>
-        <button className="notificatie-btn" onClick={() => alert('Meldingen geklikt!')}>Meldingen</button>
+        <div class = "navigatie-button-popout">
+        <button className="notificatie-btn"  ref={buttonRef} onClick={toggleNotifications}>Meldingen</button>
+
+          {showNotifications && (
+          <div className="notif-popout" ref={popoutRef}>
+          
+            <ul>
+              <li>New speeddate booked</li>
+              <li>Reminder: Interview at 15:30</li>
+              <li>Room change for Klyr</li>
+            </ul>
+          </div>
+        )}
+        </div>
         <Logo className="logo"/>
+
+        
+      
       </div>
 
       <div className="nav-bar">
