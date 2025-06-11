fbcd0a2dae2589b8f61987fa22f0cc4cb2874eb2
NicolasBorremans <nicolasborremans@hotmail.com>
2025-06-11 11:07:55 +0200

    Admin-pagina's-routes
    zo goed als alle admin pagina's gelinkt met elkaar, buiten de adminBedrijfProfiel

--- Project-Code/Programming-project/frontend/src/Admin/AdminStudentInstellingen.jsx
+++ Project-Code/Programming-project/frontend/src/Admin/AdminStudentInstellingen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Navbar from '../Components/AdminNavBar';
 
 export default function RegistratiePage() {
   const navigate = useNavigate();
