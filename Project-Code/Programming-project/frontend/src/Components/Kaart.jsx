78513463bed4bd9f9b43d5c1beb45131e746c1c1
Quinten Lauwers <99260831+Sjarel47@users.noreply.github.com>
2025-06-11 11:16:50 +0200

    verwijderen oude kaart afbeelding

--- Project-Code/Programming-project/frontend/src/Components/Kaart.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import kaartImage from '../Assets/SchoolKaart.png' 
-
-function Kaart({ width = 500, height = 'auto', alt = 'Kaart' }) {
-  return <img src={kaartImage} alt={alt} style={{ width, height }} /> 
-}
-
-export default Kaart
