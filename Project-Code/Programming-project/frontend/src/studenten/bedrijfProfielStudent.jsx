146982297495043ee133c4df345c874938d3ce7c
Argo216 <nday.roman@gmail.com>
2025-06-16 13:54:09 +0200

    bedrijf logo
    juiste link naar bedrijflogo gelegd en logos verplaatst en gelinkt in database

--- Project-Code/Programming-project/frontend/src/studenten/bedrijfProfielStudent.jsx
+++ Project-Code/Programming-project/frontend/src/studenten/bedrijfProfielStudent.jsx
@@ -154,7 +154,7 @@ export default function BedrijfProfileStudent() {
       {/* Bedrijfsprofiel */}
       <section style={{ display: 'flex', alignItems: 'center', marginBottom: '40px', paddingBottom: '20px', borderBottom: '1px solid #eee', marginTop: '70px' }}>
         <img
-          src={`/${companyData.logo_link}`}
+          src={`http://localhost:5000${companyData.logo_link}`}
           alt={`Logo van ${companyData.naam}`}
           style={{ width: '150px', height: '150px', borderRadius: '8px', objectFit: 'cover', marginRight: '30px' }}
         />
