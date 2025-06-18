8801158b0544e0cdfd3ab258af73c5856b3237df
NicolasBorremans <nicolasborremans@hotmail.com>
2025-06-18 11:49:44 +0200

    verwijder button gefixt

--- Project-Code/Programming-project/frontend/src/Admin/AdminStudentInstellingen.jsx
+++ Project-Code/Programming-project/frontend/src/Admin/AdminStudentInstellingen.jsx
@@ -132,8 +132,8 @@ export default function AdminStudentInstellingen() {
         />
 
         <button type="submit" style={buttonStyle}>Opslaan</button>
-      </form>
-      <button
+        
+        <button
         type="button"
         onClick={() => {
           if (window.confirm("Ben je zeker dat je deze student wil verwijderen?")) {
@@ -146,10 +146,12 @@ export default function AdminStudentInstellingen() {
               });
           }
         }}
-        style={{ ...buttonStyle, backgroundColor: 'red', marginTop: '10px' }}
+        style={{ ...buttonStyle, backgroundColor: 'red', marginTop: '10px', marginRight:'2px' }}
       >
         Verwijder Student
       </button>
+      </form>
+      
 
 
       <Footer />
