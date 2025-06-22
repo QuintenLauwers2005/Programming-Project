66c06a9440a2eb0d9e138afbcc9226c1a12dcdee
Quinten Lauwers <99260831+Sjarel47@users.noreply.github.com>
2025-06-22 21:06:37 +0200

    padding student registratie verwijdert

--- Project-Code/Programming-project/frontend/src/studenten/RegistratieStudenten.jsx
+++ Project-Code/Programming-project/frontend/src/studenten/RegistratieStudenten.jsx
@@ -115,7 +115,7 @@ export default function RegistratieStudentPage() {
   };
 
   return (
-    <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px' }}>
+    <div style={{ fontFamily: 'Arial, sans-serif'}}>
       <header><Navbar /></header>
 
       <section style={{ margin: '30px 0', textAlign: 'center' }}>
